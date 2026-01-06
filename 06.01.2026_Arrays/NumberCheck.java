6b708490ba51fbe2dea07f7d60622abc062f6fe3
Dikshit Singla <110338628+Dikshit2222222@users.noreply.github.com>
2026-01-06 15:01:06 +0530

    first

---
+++ 06.01.2026_Arrays/NumberCheck.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int n : arr) {
+            if (n > 0) {
+                if (n % 2 == 0)
+                    System.out.println(n + " is Positive Even");
+                else
+                    System.out.println(n + " is Positive Odd");
+            } else if (n < 0) {
+                System.out.println(n + " is Negative");
+            } else {
+                System.out.println("Zero");
+            }
+        }
+
+        if (arr[0] == arr[4])
+            System.out.println("First and last elements are equal");
+        else if (arr[0] > arr[4])
+            System.out.println("First element is greater");
+        else
+            System.out.println("Last element is greater");
+    }
+}
